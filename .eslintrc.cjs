6bced662fc48c17342771ad15b4e8d4a2c7d82f2
Sebastian Urchs <sebastian.urchs@gmail.com>
2023-12-30 23:47:25 -0500

    Added tailwind
    Including tailwind prettier plugin to sort classes
    Also moved logo into assets from public

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -13,7 +13,7 @@ module.exports = {
   ignorePatterns: ['dist', '.eslintrc.cjs'],
   parser: '@typescript-eslint/parser',
   parserOptions: {
-    project: ['./tsconfig.json', './tsconfig.node.json'],
+    project: ['./tsconfig.json', './tsconfig.node.json', './tailwind.config.js', './postcss.config.js'],
   },
   plugins: ['react-refresh'],
   rules: {
--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import { useState } from 'react';
 import reactLogo from './assets/react.svg';
-import viteLogo from '../../../../../../../vite.svg';
+import viteLogo from './assets/vite.svg';
 import './App.css';
 
 function App() {
@@ -8,7 +8,7 @@ function App() {
 
   return (
     <>
-      <div>
+      <div className="container mx-auto flex justify-center">
         <a href="https://vitejs.dev" target="_blank" rel="noreferrer">
           <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>
@@ -16,10 +16,13 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>Vite + React</h1>
+      <h1 className="inline-block bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 bg-clip-text text-4xl text-transparent">
+        Vite + React
+      </h1>
       <div className="card">
         <button
           type="button"
+          className="my-8 rounded-md bg-indigo-500 px-8 py-4 text-white shadow-lg shadow-indigo-500/50"
           onClick={() => setCount((inlineCount) => inlineCount + 1)}
         >
           count is {count}
